aaa3410b10843824bc059582a6ed7b38cfcc29ac
kuzivashe <kuzibaye@gmail.com>
2023-04-22 05:40:56 +0000

    c route

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -3,7 +3,7 @@
 """
 
 
-from flask import Flask, escape
+from flask import Flask, escape, render_template
 app = Flask(__name__)
 # condition strict_slashes=False
 app.url_map.strict_slashes = False
@@ -24,14 +24,36 @@ def no_hello():
 
 
 @app.route('/c/<text>')
-def C_coment(text='C'):
+def C_coment(text='Python'):
     """ print a comment related to C
     """
     text = text.replace('_', ' ')
     return ("C {}".format(escape(text)))
 
 
+@app.route('/python')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """ commentary for python
+    """
+    text = text.replace('_', ' ')
+    return ("Python {}".format(escape(text)))
+
+
+@app.route('/number/<int:n>')
+def integer(n):
+    """ print a integer number
+    """
+    return ("{} is a number".format(n))
+
+
+@app.route('/number_template/<int:n>')
+def render(n):
+    """ first render html, by default it search in templates folder
+    """
+    return (render_template('5-number.html', n=n))
+
+
 if __name__ == "__main__":
     # config the run
     app.run(host='0.0.0.0', port=5000)
-
